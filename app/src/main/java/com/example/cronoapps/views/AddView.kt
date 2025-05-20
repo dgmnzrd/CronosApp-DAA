dc700f8bd60fcfd2cf7da93b6f1b8994cd00626d
Diego Muñoz Rede <dmrede21@outlook.com>
2025-05-20 11:14:42 -0600

    Se agregan iconos para los botones de accion del cronometro.

--- app/src/main/java/com/example/cronoapps/views/AddView.kt
+++ app/src/main/java/com/example/cronoapps/views/AddView.kt
@@ -1,14 +1,13 @@
 package com.example.cronoapps.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,14 +15,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableTarget
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.cronoapps.R
+import com.example.cronoapps.components.CircleButton
 import com.example.cronoapps.components.MainIconButton
 import com.example.cronoapps.components.MainTitle
 import com.example.cronoapps.components.formatTiempo
@@ -71,8 +72,42 @@ fun ContentAddView(it:PaddingValues,navController: NavController,cronometroVM: C
             fontWeight = FontWeight.Bold
         )
 
-        Button(onClick ={cronometroVM.iniciar()}){
-            Text(text="Iniciar")
+        Row(
+            horizontalArrangement=Arrangement.Center,
+            modifier=Modifier.padding(vertical=16.dp)
+        ){
+            //Iniciar
+            CircleButton(
+                icon= painterResource(id=R.drawable.play),
+                enabled=!state.cronometroActivo
+            ) {
+                cronometroVM.iniciar()
+            }
+
+            //Pausar
+            CircleButton(
+                icon=painterResource(id=R.drawable.pause),
+                enabled=state.cronometroActivo
+            ) {
+                cronometroVM.pausar()
+            }
+            //Detener
+            CircleButton(
+                icon=painterResource(id=R.drawable.stop),
+                enabled=!state.cronometroActivo
+            ) {
+                cronometroVM.detener()
+            }
+
+            //Mostrar Guardar
+            CircleButton(
+                icon=painterResource(id=R.drawable.save),
+                enabled=state.showSaveButton
+            ) {
+                cronometroVM.showTextField()
+            }
         }
+
+
     }
 }
